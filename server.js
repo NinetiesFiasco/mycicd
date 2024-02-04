24eb53ee656aa7cfdff45850fdca23d10ddb5adc
Anton Seredin <seredin.anton2014@gmail.com>
2024-02-05 00:43:03 +0200

    Initial

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const MESSAGE = process.env.MESSAGE || 'no branch'
+
+app.get('/', (req, res) => {
+  res.send(`Hi in my CI CD git hub pipeline! branch: ${MESSAGE}`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
